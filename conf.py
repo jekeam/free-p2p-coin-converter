14561654bde9be706764a3f0b1c2ec14fe6fb48f
Alexander Savinykh <suinegne@gmail.com>
2020-06-17 15:55:34 +0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-TOKEN = '702332140:AAEl8oW7493bWcaRvQA2DGPl19jL1b_BL1Q'
-proxy = 'http://Sela89823703090:H5f7LlK@176.114.8.78:45785'
+TOKEN = ''
+proxy = 'http://'
 REQUEST_KWARGS = {
     'proxy_url': proxy,
     'read_timeout': 60,
